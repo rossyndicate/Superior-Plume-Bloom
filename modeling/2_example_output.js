1f860354c1a19313f22cbdbb7852d39ded250a07
B Steele <B.Steele@colostate.edu>
2023-07-05 14:14:01 -0600

    add GTB and palette to example

--- modeling/2_example_output.js
+++ modeling/2_example_output.js
@@ -1,5 +1,5 @@
 var sat_img = ee.Image('projects/ee-ross-superior/assets/example_output/L9_2022-05-05');
-var sat_class = ee.Image('projects/ee-ross-superior/assets/example_output/L9_2022-05-05_quickclass');
+var sat_class = ee.Image('projects/ee-ross-superior/assets/example_output/L9_2022-05-05_quickclass_GTB');
 
 var sat_viz = {
   bands: ['SR_B4', 'SR_B3', 'SR_B2'],
@@ -11,8 +11,9 @@ var class_viz = {
   bands: ['classification'],
   min: 0,
   max: 4,
+  palette: ['#ffffff', '#7ff6ff', '#9c7238', '#d63000', '#98ff00']
 };
 
 Map.addLayer(sat_img, sat_viz, 'Satellite Image');
 Map.addLayer(sat_class, class_viz, 'Classified Image');
-Map.centerObject(sat_img, 10)
+Map.centerObject(sat_img, 10);
