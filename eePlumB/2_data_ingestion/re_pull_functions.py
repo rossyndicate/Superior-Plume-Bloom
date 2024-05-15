3c2e5f2b6ed8f3d61e109abb1097359aff9f2a1f
B Steele <B.Steele@colostate.edu>
2024-05-15 11:45:01 -0600

    move repull from main to newadds branch

--- eePlumB/2_data_ingestion/re_pull_functions.py
+++ eePlumB/2_data_ingestion/re_pull_functions.py
@@ -27,11 +27,13 @@ def applyScaleFactors(image):
     """
     opticalBands = image.select("SR_B.").multiply(0.0000275).add(-0.2)
     thermalBands = image.select("ST_B.").multiply(0.00341802).add(149.0)
-    opac = image.select('SR_ATMOS_OPACITY').multiply(0.0001)
+    opac = image.select('SR_ATMOS_OPACITY').multiply(0.001)
+    cloudDist = image.select("ST_CDIST").multiply(0.01)
     return (image
         .addBands(opticalBands, None, True)
         .addBands(thermalBands, None, True)
-        .addBands(opac, None, True))
+        .addBands(opac, None, True)
+        .addBands(cloudDist, None, True))
 
 # function to apply scaling factors to LS8/9
 def applyScaleFactors_89(image):
@@ -46,9 +48,11 @@ def applyScaleFactors_89(image):
     """
     opticalBands = image.select("SR_B.").multiply(0.0000275).add(-0.2)
     thermalBands = image.select("ST_B..").multiply(0.00341802).add(149.0)
+    cloudDist = image.select("ST_CDIST").multiply(0.01)
     return (image
         .addBands(opticalBands, None, True)
-        .addBands(thermalBands, None, True))
+        .addBands(thermalBands, None, True)
+        .addBands(cloudDist, None, True))
 
 def rename_S2_band(bn):
   """
@@ -86,13 +90,13 @@ def applyScaleFactors_S2(image):
 # function to mask saturated pixels
 def apply_radsat_mask(image):
     """
-    Masks saturated pixels in an image.
+    Masks saturated pixels in a Landsat image.
 
     Args:
-        image (ee.Image): The earth engine image to mask.
+        image (ee.Image): The Landast earth engine image to mask.
 
     Returns:
-        ee.Image: The earth engine masked image.
+        ee.Image: The Landast earth engine masked image.
     """
     radsat = image.select('QA_RADSAT').eq(0)
     return image.updateMask(radsat)
@@ -102,6 +106,7 @@ def apply_radsat_mask(image):
 def apply_sat_defect_mask(image):
     """
     Masks saturated and defective pixels in an image according to the SCL band
+    in Sentinel 2 images
 
     Args:
         image (ee.Image): The earth engine Sentinel image to mask.
@@ -111,6 +116,26 @@ def apply_sat_defect_mask(image):
     """
     sat_def = image.select('SCL').neq(1)
     return image.updateMask(sat_def)
+  
+def add_qa_info_s2(image):
+  scl = image.select('SCL')
+  dark = scl.eq(2).rename('dark_pixel')
+  cloudShad = scl.eq(3).rename('cloud_shadow')
+  water = scl.eq(6).rename('water')
+  lowProbCloud = scl.eq(7).rename('low_prob_cloud')
+  medProbCloud = scl.eq(8).rename('med_prob_cloud')
+  hiProbCloud = scl.eq(9).rename('hi_prob_cloud')
+  cirrus = scl.eq(10).rename('cirrus_scl')
+  snowice = scl.eq(11).rename('snow_ice')
+  return (image
+    .addBands(dark)
+    .addBands(cloudShad)
+    .addBands(water)
+    .addBands(lowProbCloud)
+    .addBands(medProbCloud)
+    .addBands(hiProbCloud)
+    .addBands(cirrus)
+    .addBands(snowice))
 
 
 # function to add image mission-date to properties of an image
@@ -177,29 +202,74 @@ def extract_qa_bits(qa_band, start_bit, end_bit, band_name):
     # (0 or 1 for single bit,  0-3 or 0-N for multiple bits)
     .rightShift(start_bit))
 
+# function to flag high aerosol pixels
+def flag_high_aerosol(image):
+  qa_aero = image.select("SR_QA_AEROSOL")
+  aero = extract_qa_bits(qa_aero, 6, 8, 'aero_level')
+  return image.addBands(aero)
+ 
+# Bit 1: Dilated Cloud
+# Bits 8-9: Cloud Confidence
+# 0: None
+# 1: Low
+# 2: Medium
+# 3: High
+# Bits 10-11: Cloud Shadow Confidence
+# 0: None
+# 1: Low
+# 2: Medium
+# 3: High
+# Bits 12-13: Snow/Ice Confidence
+# 0: None
+# 1: Low
+# 2: Medium
+# 3: High
+# Bits 14-15: Cirrus Confidence
+# 0: None
+# 1: Low
+# 2: Medium
+# 3: High
 
 # function to flag cirrus pixels
-def flag_cirrus_conf(image):
+def flag_qa_conf(image):
   """
   Flags pixels in an image where the cirrus confidence is high and provideds
-  the classified cirurs confidence for Landsat 8 and 9.
+  the classified cirurs confidence for Landsat images.
 
   Args:
       image (ee.Image): The earth engine image to flag the pixels in.
 
   Returns:
-      ee.Image: The image with the added bands for high cirrus confidence and cirrus confidence.
+      ee.Image: The image with the added band for qa bit confidence, 1 is low, 3 
+      is high, except dialated cloud which is 0 none, 1 present
   """
   qa = image.select('QA_PIXEL')
   # where cirrus confidence is high
-  cirrus_high = qa.bitwiseAnd(1 << 2).rename('high_conf_cirrus')
   cirrus_conf = extract_qa_bits(qa, 14, 16, 'cirrus_conf')
-  return image.addBands(cirrus_high).addBands(cirrus_conf)
+  snowice_conf = extract_qa_bits(qa, 12, 14, 'snowice_conf')
+  cloudshad_conf = extract_qa_bits(qa, 10, 12, 'cloudshad_conf')
+  cloud_conf = extract_qa_bits(qa, 8, 10, 'cloud_conf')
+  dialated = extract_qa_bits(qa, 1, 2, 'dialated_cloud')
+  return (image
+    .addBands(cirrus_conf)
+    .addBands(snowice_conf)
+    .addBands(cloudshad_conf)
+    .addBands(cloud_conf)
+    .addBands(dialated))
+    
+
+# Bit 10: Opaque clouds
+# 0: No opaque clouds
+# 1: Opaque clouds present
+# Bit 11: Cirrus clouds
+# 0: No cirrus clouds
+# 1: Cirrus clouds present
 
 # function to flag cirrus and opaque cloud pixels in Sentinel 2
 def flag_cirrus_opaque(image):
   """
-  Flags pixels in an image where the the QA60 band indicates cirrus or opaque cloud presence.
+  Flags pixels in an image where the the QA60 band indicates cirrus or opaque 
+  cloud presence in Sentinel 2 images
 
   Args:
       image (ee.Image): The earth engine image to flag the pixels in.
@@ -209,6 +279,18 @@ def flag_cirrus_opaque(image):
   """
   qa = image.select('QA60')
   # where cirrus confidence is high
-  cirrus_high = qa.bitwiseAnd(1 << 11).rename('cirrus')
-  cirrus_conf = qa.bitwiseAnd(1 << 10).rename('opaque_cloud')
-  return image.addBands(cirrus_high).addBands(cirrus_conf)
+  cirrus = extract_qa_bits(qa, 11, 12, 'cirrus')
+  opaque = extract_qa_bits(qa, 10, 11, 'opaque')
+  return image.addBands(cirrus).addBands(opaque)
+
+## Remove geometries
+def remove_geo(image):
+  """ Funciton to remove the geometry from an ee.Image
+  
+  Args:
+      image: ee.Image of an ee.ImageCollection
+      
+  Returns:
+      ee.Image with the geometry removed
+  """
+  return image.setGeometry(None)
